a8bfea567f22d52915bfe3a04cb4be9244792b75
Vishnuprasad Kuntar <kuntarvishnuprasad41@gmail.com>
2023-11-24 12:35:08 +0530

    fix: empty response if promise resolved

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -8,75 +8,75 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const User = sequelize.define('user', {
     firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     middleName: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     mobile: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      
+        type: DataTypes.STRING,
+        allowNull: true,
+
     },
     email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-      validate: {
-        isEmail: true,
-      },
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        },
     },
     password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        min: 8,
-      },
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            min: 8,
+        },
     },
 
     timestamps: false,
 
     // If don't want createdAt
     createdAt: false,
-  
+
     // If don't want updatedAt
     updatedAt: false,
 
-  });
+});
 
 
-  const storeUser = async function(obj){
+const storeUser = async function (obj) {
     const user = new User(
         {
-        firstName: obj.firstName,
-        middleName: obj.middleName,
-        lastName: obj.lastName,
-        mobile: obj.mobile,
-        email: obj.email,
-        password: obj.password,
+            firstName: obj.firstName,
+            middleName: obj.middleName,
+            lastName: obj.lastName,
+            mobile: obj.mobile,
+            email: obj.email,
+            password: obj.password,
         }
     );
 
 
     return new Promise(async (resolve, reject) => {
         try {
-          await user.save();
-          resolve();
+            await user.save();
+            resolve(user);
         } catch (err) {
-          reject(err);
+            reject(err);
         }
-      });
-    
-  }
+    });
+
+}
 
 
 
 
 
-module.exports = {User,storeUser}
+module.exports = { User, storeUser }
